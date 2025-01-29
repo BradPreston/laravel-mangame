64dd8d805cb3ff2e8a1ccf3a86c1754869a2301f
brad preston <bap5393@gmail.com>
2025-01-28 21:38:22 -0500

    Add form to Create User page to send requests to the User Controller

--- resources/js/Pages/User/Create.vue
+++ resources/js/Pages/User/Create.vue
@@ -1,5 +1,37 @@
-<script setup></script>
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    name: null,
+    email: null,
+    password: null,
+});
+</script>
 
 <template>
     <h1>Create User page</h1>
+    <form @submit.prevent="form.post('/user')">
+        <input
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            v-model="form.name"
+        />
+        <div v-if="form.errors.name">{{ form.errors.name }}</div>
+        <input
+            type="email"
+            name="email"
+            placeholder="john.doe@email.com"
+            v-model="form.email"
+        />
+        <div v-if="form.errors.email">{{ form.errors.email }}</div>
+        <input
+            type="password"
+            name="password"
+            placeholder="password"
+            v-model="form.password"
+        />
+        <div v-if="form.errors.password">{{ form.errors.password }}</div>
+        <button>Submit</button>
+    </form>
 </template>
