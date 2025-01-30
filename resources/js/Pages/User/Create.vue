3618b311a6c49a9636a940d93858ca667660ce9d
brad preston <bap5393@gmail.com>
2025-01-29 22:48:21 -0500

    Update styles and handle showing error alert on form error

--- resources/js/Pages/User/Create.vue
+++ resources/js/Pages/User/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useForm } from "@inertiajs/vue3";
 import { Head } from "@inertiajs/vue3";
+import { ref } from "vue";
 
 const form = useForm({
     name: null,
@@ -8,9 +9,17 @@ const form = useForm({
     password: null,
 });
 
+const totalErrors = ref(0);
+
 function handleSubmit() {
     form.post("/user", {
-        onSuccess: () => form.reset(),
+        onError: () => {
+            totalErrors.value = Object.keys(form.errors).length;
+        },
+        onSuccess: () => {
+            form.reset();
+            totalErrors.value = 0;
+        },
     });
 }
 </script>
@@ -20,7 +29,7 @@ function handleSubmit() {
         <Head title="Create New User - Mangame" />
         <h1 class="text-center">Create User page</h1>
         <form
-            class="flex flex-col mx-auto max-w-xs"
+            class="flex flex-col mx-auto max-w-sm"
             @submit.prevent="handleSubmit"
         >
             <label
@@ -30,7 +39,7 @@ function handleSubmit() {
                 Full Name*
             </label>
             <input
-                :class="`input input-bordered w-full max-w-xs mb-4 ${
+                :class="`input input-bordered w-full max-w-sm mb-4 ${
                     form.errors.name && 'border-red-500'
                 }`"
                 id="user[name]"
@@ -46,7 +55,7 @@ function handleSubmit() {
                 Email*
             </label>
             <input
-                :class="`input input-bordered w-full max-w-xs mb-4 ${
+                :class="`input input-bordered w-full max-w-sm mb-4 ${
                     form.errors.email && 'border-red-500'
                 }`"
                 type="email"
@@ -62,7 +71,7 @@ function handleSubmit() {
                 Password*
             </label>
             <input
-                :class="`input input-bordered w-full max-w-xs mb-4 ${
+                :class="`input input-bordered w-full max-w-sm mb-4 ${
                     form.errors.password && 'border-red-500'
                 }`"
                 type="password"
@@ -71,7 +80,27 @@ function handleSubmit() {
                 placeholder="password"
                 v-model="form.password"
             />
-            <button class="btn btn-primary max-w-xs">Submit</button>
+            <button class="btn btn-primary max-w-sm">Submit</button>
         </form>
+        <div
+            role="alert"
+            class="alert alert-error mt-4 max-w-sm mx-auto"
+            v-if="totalErrors"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+            <span>Make sure all required fields are field out</span>
+        </div>
     </div>
 </template>
