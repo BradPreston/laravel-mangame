f263cec6d61a325ee0b7a67eec74ea6379ec6d77
brad preston <bap5393@gmail.com>
2025-01-29 22:02:00 -0500

    Install tailwind typography package and config the form with prose

--- resources/js/Pages/User/Create.vue
+++ resources/js/Pages/User/Create.vue
@@ -10,30 +10,32 @@ const form = useForm({
 </script>
 
 <template>
-    <Head title="Create New User - Mangame" />
-    <h1>Create User page</h1>
-    <form @submit.prevent="form.post('/user')">
-        <input
-            type="text"
-            name="name"
-            placeholder="John Doe"
-            v-model="form.name"
-        />
-        <div v-if="form.errors.name">{{ form.errors.name }}</div>
-        <input
-            type="email"
-            name="email"
-            placeholder="john.doe@email.com"
-            v-model="form.email"
-        />
-        <div v-if="form.errors.email">{{ form.errors.email }}</div>
-        <input
-            type="password"
-            name="password"
-            placeholder="password"
-            v-model="form.password"
-        />
-        <div v-if="form.errors.password">{{ form.errors.password }}</div>
-        <button>Submit</button>
-    </form>
+    <div class="prose">
+        <Head title="Create New User - Mangame" />
+        <h1>Create User page</h1>
+        <form @submit.prevent="form.post('/user')">
+            <input
+                type="text"
+                name="name"
+                placeholder="John Doe"
+                v-model="form.name"
+            />
+            <div v-if="form.errors.name">{{ form.errors.name }}</div>
+            <input
+                type="email"
+                name="email"
+                placeholder="john.doe@email.com"
+                v-model="form.email"
+            />
+            <div v-if="form.errors.email">{{ form.errors.email }}</div>
+            <input
+                type="password"
+                name="password"
+                placeholder="password"
+                v-model="form.password"
+            />
+            <div v-if="form.errors.password">{{ form.errors.password }}</div>
+            <button>Submit</button>
+        </form>
+    </div>
 </template>
