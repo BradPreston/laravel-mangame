c0363f839794d93d9ad6553a9f3a3bf391324b96
bradpreston <bap5393@gmail.com>
2025-02-01 17:08:04 -0500

    Added a single user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            "first_name" => "Test",
+            "last_name" => "User",
+            "email" => "test@email.com",
+            "password" => Hash::make('Testpass1')
         ]);
     }
 }
