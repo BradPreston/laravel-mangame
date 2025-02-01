67c4b46876c0df9590699efa9ff91c9a9f031923
brad preston <bap5393@gmail.com>
2025-02-01 16:37:25 -0500

    Update user table to have first_name and last_name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
@@ -15,9 +16,10 @@ class UserController extends Controller
     public function store(Request $request)
     {
         $rules = [
-            "name" => "required",
+            "first_name" => "required",
+            "last_name" => "required",
             "password" => "required",
-            "email" => "required"
+            "email" => "required|unique:users"
         ];
         $validations = [
             "required" => "the :attribute field is required"
@@ -26,6 +28,13 @@ class UserController extends Controller
         if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
         }
+        $user = User::create([
+            "first_name" => $request->input('first_name'),
+            "last_name" => $request->input('last_name'),
+            "email" => $request->input('email'),
+            "password" => $request->input('password'),
+        ]);
+
         return redirect()->back();
     }
 }
