7680dafd647387c468ace585a06b228439bf8662
brad preston <bap5393@gmail.com>
2025-02-02 16:00:01 -0500

    Create layout component and use it in app as default + inertia user data

---
+++ resources/js/Layout.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { Link, usePage } from "@inertiajs/vue3";
+import { computed } from "vue";
+import Header from "./Components/UI/Header.vue";
+
+const page = usePage();
+
+const user = computed(() => page.props.auth.user);
+
+const navLinks = [
+    {
+        href: "/",
+        name: "Home",
+    },
+];
+
+const loggedInUserLinks = [
+    {
+        href: "/account",
+        name: "Account",
+    },
+    {
+        href: "/logout",
+        name: "Logout",
+    },
+];
+
+const loggedOutUserLinks = [
+    {
+        href: "/account/login",
+        name: "Login",
+    },
+    {
+        href: "/account/register",
+        name: "Register",
+    },
+];
+</script>
+
+<template>
+    <main>
+        <div v-if="user">
+            <Header :navLinks="navLinks" :userLinks="loggedInUserLinks" />
+        </div>
+        <div v-else>
+            <Header :navLinks="navLinks" :userLinks="loggedOutUserLinks" />
+        </div>
+        <div class="prose container mx-auto">
+            <slot />
+        </div>
+    </main>
+</template>
